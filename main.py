eb7a46971cb09dc413b0c5eff47dc29e562200d0
choumar2001 <choumar2001@gmail.com>
2023-03-24 12:41:54 +0100

    Added a logo and a footer for the company

--- main.py
+++ main.py
@@ -43,4 +43,26 @@ for file in filepaths:
         pdf.cell(w=40, h=10, txt=str(row["amount_purchased"]), border=1)
         pdf.cell(w=30, h=10, txt=str(row["price_per_unit"]), border=1)
         pdf.cell(w=30, h=10, txt=str(row["total_price"]), border=1, ln=1)
+
+    # getting total of the price
+    total_sum = df["total_price"].sum()
+    pdf.set_font(family="Times", size=12)
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=10, txt="", border=1)
+    pdf.cell(w=50, h=10, txt="", border=1)
+    pdf.cell(w=40, h=10, txt="", border=1)
+    pdf.cell(w=30, h=10, txt="", border=1)
+    pdf.cell(w=30, h=10, txt=str(total_sum), border=1, ln=1)
+
+    # add total sum sentence at the end
+    pdf.set_font(family="Times", size=10, style="B")
+    pdf.set_text_color(0, 0, 0)
+    pdf.cell(w=30, h=8, txt=f"The total price is {total_sum}", ln=1)
+
+    # add company logo and name
+    pdf.set_font(family="Times", size=14, style="B")
+    pdf.cell(w=25, h=8, txt=f"PythonHow")
+    pdf.image("pythonhow.png", w=10)
+
+    # generates the pdf
     pdf.output(f"PDFs/{filename}.pdf")
