34bfcdcd76d9f9f0205bd3022b108d8b1c8b4850
choumar2001 <choumar2001@gmail.com>
2023-03-24 09:57:41 +0100

    Invoice

--- main.py
+++ main.py
@@ -6,13 +6,19 @@ from pathlib import Path
 
 # access csv files
 filepaths = glob("Invoices/*.xlsx")
-print(filepaths)
+
 for file in filepaths:
+    # read Excel files
     df = pd.read_excel(file, sheet_name="Sheet 1")
+
+    # add Excel file information into a pdf
     pdf = fpdf.FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
+
+    #
     filename = Path(file).stem
     invoice_nr = filename.split("-")[0]
+
     pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=f"Invoice nr. {invoice_nr}", ln=1)
+    pdf.cell(w=50, h=8, txt=f"Invoice nr. {invoice_nr}")
     pdf.output(f"PDFs/{invoice_nr}.pdf")
